e0f31b403819d03dac0725db941be9c1e7c61f2e
pablo <latorrepablo775@gmail.com>
2026-05-27 23:17:32 -0400

    caso dos numeros

--- src/sumador.js
+++ src/sumador.js
@@ -2,6 +2,12 @@ function sumar(cadena) {
   if (cadena === "") {
     return 0;
   }
+  
+  if (cadena.includes(",")) {
+    const numeros = cadena.split(",");
+    return parseInt(numeros[0]) + parseInt(numeros[1]);
+  }
+
   return parseInt(cadena);
 }
 
--- src/sumador.test.js
+++ src/sumador.test.js
@@ -7,5 +7,7 @@ describe("Calculadora de Cadenas", () => {
   it("deberia retornar el mismo numero si la cadena tiene un solo numero", () => {
     expect(sumar("2")).toEqual(2);
   });
-  
+  it("deberia retornar la suma de dos numeros separados por coma", () => {
+    expect(sumar("1,2")).toEqual(3);
+  });
 });
