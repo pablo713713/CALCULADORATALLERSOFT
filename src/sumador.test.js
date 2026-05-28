75a75f2516b4e13e74941a49b38a93deb569d998
pablo <latorrepablo775@gmail.com>
2026-05-27 23:15:19 -0400

    un numero

--- src/sumador.test.js
+++ src/sumador.test.js
@@ -4,5 +4,8 @@ describe("Calculadora de Cadenas", () => {
   it("deberia retornar 0 para una cadena vacia", () => {
     expect(sumar("")).toEqual(0);
   });
+  it("deberia retornar el mismo numero si la cadena tiene un solo numero", () => {
+    expect(sumar("2")).toEqual(2);
+  });
   
 });
